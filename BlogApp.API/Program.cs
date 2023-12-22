ec4afc504ab9544da6aa87d2d48a1df9414c46bc
hmaleyka <maleykaheybatova1011@gmail.com>
2023-12-22 17:09:31 +0400

    fluent|validation

--- BlogApp.API/Program.cs
+++ BlogApp.API/Program.cs
@@ -1,9 +1,12 @@
+using BlogApp.Business.DTOs.BrandDto;
 using BlogApp.Business.Services.Implementations;
 using BlogApp.Business.Services.Interfaces;
 using BlogApp.DAL.Context;
 using BlogApp.DAL.Repositories.Implementations;
 using BlogApp.DAL.Repositories.Interfaces;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IBrandRepository, BrandRepository>();
 builder.Services.AddScoped<IBrandService , BrandService>();
 builder.Services.AddControllers();
+
+builder.Services.AddControllers().AddFluentValidation(opt =>
+{
+    opt.RegisterValidatorsFromAssembly(typeof(Validation).Assembly);
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- BlogApp.Business/DTOs/BrandDto/CreateBrandDto.cs
+++ BlogApp.Business/DTOs/BrandDto/CreateBrandDto.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using BlogApp.Core.Entities.Common;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +12,17 @@ namespace BlogApp.Business.DTOs.BrandDto
     public  class CreateBrandDto
     {
         public string Name { get; set; }
+    }
+
+    public class Validation : AbstractValidator<CreateBrandDto>
+    {
+        public Validation()
+        {
+            RuleFor(x => x.Name).NotEmpty()
+                .WithMessage("Name should not be empty")
+                .MinimumLength(3).WithMessage("Name should have at least 3 characters.")
+                .MaximumLength(50).WithMessage("Name cannot exceed 50 characters.");
+
+        }
     }
 }
